d11042a34a7befd03966ca77b4accc228bcd4b11
Connor Leigh-Smith <connorbob21@gmail.com>
2016-10-16 13:59:25 +0100

    Removed transform on video making image reversed

--- qr-scanner.js
+++ qr-scanner.js
@@ -22,7 +22,7 @@ angular.module('qrScanner', ["ng"]).directive('qrScanner', ['$interval', '$windo
       video.setAttribute('width', width);
       video.setAttribute('height', height);
       video.setAttribute('autoplay', '');
-      video.setAttribute('style', '-moz-transform:rotateY(-180deg);-webkit-transform:rotateY(-180deg);transform:rotateY(-180deg);');
+      //video.setAttribute('style', '-moz-transform:rotateY(-180deg);-webkit-transform:rotateY(-180deg);transform:rotateY(-180deg);');
       var canvas = $window.document.createElement('canvas');
       canvas.setAttribute('id', 'qr-canvas');
       canvas.setAttribute('width', width);
